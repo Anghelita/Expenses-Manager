b0a27a66bc9e9242b032b603feb508b0e09312d3
avramus <avramandrei9666@gmail.com>
2018-01-14 19:54:21 +0200

    jj

--- Expenses Manager/HramulModel.Designer.cs
+++ Expenses Manager/HramulModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\anghe\source\repos\Expenses Manager\Expenses Manager\HramulModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\avram\Source\Repos\Expenses-Manager\Expenses Manager\HramulModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
